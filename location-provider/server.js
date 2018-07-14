a2b861bcf8fd996d4f29148445f3243e15487f3f
chiragrc <chirag.rc@swiggy.in>
2018-07-14 19:10:56 +0530

    adding location provider service

---
+++ location-provider/server.js
@@ -0,0 +1,39 @@
+
+var logger = require("./logger").logger;
+
+var http = require("http");
+
+var express = require("express");
+
+
+var app = express();
+
+// Body Parser Middleware
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: "20mb"}));
+app.use(bodyParser.urlencoded({ extended : true }));
+
+
+app.get("/", function(req, res){
+    res.status(200).send("Success");
+});
+
+// Routing
+var routes = require("./routers").routes;
+app.use('/', routes);
+console.log("Server started 1");
+// Error Handling
+app.use(logErrors);
+function logErrors(err, req, res, next) {
+    logger.error("%j", {
+        "location": "server",
+        "errorStack": err.stack
+    });
+    res.status(500).send();
+}
+
+var server;
+server = http.createServer(app);
+server.listen(8003);
+console.log("Server started");
+
